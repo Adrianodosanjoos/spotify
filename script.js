d75f0714063c0d093e47156ab35e3c110c59bf74
adrianodosanjoos <adriano.luizysa@gmail.com>
2024-09-13 16:35:46 -0300

    atualização

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     const artistsDate = [
-        {name: 'Ivandro Artista', Image: '.img/Ivandro-Artista.jpeg' },
-        {name: 'Dillaz Artista', Image: './img/Dillaz Artista.jpeg' },
-        {name: 'Coldplay Artista', Image: './img/Coldplay Artista.jpeg' },
-        {name: 'Billie Elish Artista', Image: './img/Billie Elilish Artista.jpeg' },
-        {name: 'Slow J Artista', Image: './img/slow J Artista.jpeg' },
-        {name: 'The Weeknd Artista', Image: '.img/The weeknd Artista.jpeg' },
-        {name: 'Pedro Sampaio Artista', Image: '.img/Pedro Sampaio Artista.jpeg' }
+        {name: 'Ivandro Artista', image: './img/Ivandro-Artista.jpeg' },
+        {name: 'Dillaz Artista', image: './img/Dillaz-Artista.jpeg' },
+        {name: 'Coldplay Artista', image: './img/Coldplay-Artista.jpeg' },
+        {name: 'Billie Elish Artista', image: './img/Billie-Elilish-Artista.jpeg' },
+        {name: 'Slow J Artista', image: './img/slow-J-Artista.jpeg' },
+        {name: 'The Weeknd Artista', image: './img/The-weeknd-Artista.jpeg' },
+        {name: 'Pedro Sampaio Artista', image: './img/Pedro-Sampaio-Artista.jpeg' }
     ];
     
     const albumssDate = [
-        {name: 'Afro Fado', artist:' Slow J', Image: './img/Afro Fado artist slow J.jpeg' },
-        {name: 'Short nSweet', artist:'Capenter', Image: './img/Capenter.jpeg' },
-        {name: 'HIT ME HARD AND SOFT', artist:'Billie Elish', Image: './img/HIT ME HARD AND SOFT artist Billie Eilish.jpeg' },
-        {name: 'Trovador', artist:'Ivandro', Image: './img/Trovador artist Ivandro.jpeg' },
-        {name: 'Utopia', artist:'Travis Scott', Image: './img/UTOPIA artist Travis Scott.jpeg' },
-        {name: 'O Próprio', artist:'Dillaz', Image: './img/O Próprio artist Dillaz.jpeg' },
+        {name: 'Afro Fado', artist:' Slow J', image: './img/Pedro-Sampaio-Artista.jpeg' },
+        {name: 'Short nSweet', artist:'Capenter', image: './img/Capenter.jpeg' },
+        {name: 'HIT ME HARD AND SOFT', artist:'Billie Elish', image: './img/HIT-ME-HARD-AND-SOFT-artist-Billie-Eilish.jpeg' },
+        {name: 'Trovador', artist:'Ivandro', image: './img/Trovador-artist-Ivandro.jpeg' },
+        {name: 'Utopia', artist:'Travis Scott', image: './img/UTOPIA-artist-Travis-Scott.jpeg' },
+        {name: 'O Próprio', artist:'Dillaz', image: './img/O-Próprio-artist-Dillaz.jpeg' },
     ];
 
     const artistGrid = document.querySelector('.artists-grid')
